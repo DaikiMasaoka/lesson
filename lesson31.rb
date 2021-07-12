f686352b00aae94357c71c8c2fb2c9c27ab59e0e
DaikiMasaoka <masadai.29@icloud.com>
2021-07-12 00:51:28 +0000

    lesson31.rb

--- lesson31.rb
+++ lesson31.rb
@@ -2,11 +2,11 @@
 # a≤x≤b,c≤y≤dを満たす整数
 # x,yについて、xyの最大値はいくつですか。
 
-# a,b,c,d = gets.chomp.split.map(&:to_i)
+a,b,c,d = gets.chomp.split.map(&:to_i)
 
-# number_1 = a * c
-# number_2 = a * d
-# number_3 = b * c
-# number_4 = b * d
+number_1 = a * c
+number_2 = a * d
+number_3 = b * c
+number_4 = b * d
 
-# p [number_1,number_2,number_3,number_4].max
+p [number_1,number_2,number_3,number_4].max
